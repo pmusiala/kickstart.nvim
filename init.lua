f8484e694d976ee46794382f8a4d71ca6d12ca81
Patryk Musiala <p.musiala@sportradar.com>
2024-03-25 13:50:13 +0100

    add autoclose

--- init.lua
+++ init.lua
@@ -229,6 +229,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'm4xshen/autoclose.nvim', -- autoclose brackets and other things
   {
     'nvim-tree/nvim-tree.lua',
     opts = {
